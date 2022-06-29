b45c11c49b6d0e8dad3a22a603305e634e1d9fe5
RoniDeringer <ronideringer@gmail.com>
2022-06-29 20:32:26 -0300

    create repository (29/06/22)

---
+++ css/style.php
@@ -0,0 +1,46 @@
+<style>
+html,body{ 
+  font-family: sans-serif;
+  background-color: #333;
+  color:#eee;
+  padding-bottom:50%;
+}
+h2{
+   color:#458688;
+  font-size: 23px;
+}
+hr{
+  margin-top: 40px;
+  margin-bottom: 30px;
+  opacity: 0.23;
+}
+pre{
+  background-color: #444;
+  padding:10px;
+  font-size: 20px;
+  color: #ecf0f1;
+}
+
+strong{
+  font-style: italic;
+  color: #ecf0f1  
+}
+
+entrada{
+  font-style: italic;
+  color: aqua;  
+}
+aceito{
+  font-size: 30px;
+  font-style: bold;
+  background-color: #a3a3a3;
+  color: #006400;
+}
+
+naceito{
+  font-size: 30px;
+  font-style: bold;
+  background-color: #a3a3a3;
+  color: #8B0000;
+}
+</style>
